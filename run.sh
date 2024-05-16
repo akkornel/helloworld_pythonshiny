39bb01f05b754a58956da39c2d0c47e2728b299f
A. Karl Kornel <akkornel@stanford.edu>
2024-05-15 17:54:34 -0700

    Update run script to relay signals

--- run.sh
+++ run.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# Trap SIGTERM
+_sigterm() {
+    echo "Run script caught signal.  Telling programs to exit."
+    kill -TERM "$shiny_pid"
+    kill -TERM "$nginx_pid"
+}
+trap _sigterm SIGTERM
+trap _sigterm SIGINT
+
 # First, run the Shiny app on port 8001
 PYTHONPATH=/usr/local/src/myscripts/app DEBUG=TRUE /usr/local/src/myscripts/bin/pipenv run uvicorn --host=0.0.0.0 --port=8001 --log-level=debug app:app &
+shiny_pid=$!
 
 # Next, run the proxy server on port 8000
 nginx -g "daemon off;" &
+nginx_pid=$!
 
 # Wait for both things to exit, then end
 wait
